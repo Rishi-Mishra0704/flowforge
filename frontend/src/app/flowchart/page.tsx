15c1d654dafcb02550ba3bd649ca7335c730de2f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-12-02 16:06:02 +0530

    some stuff

---
+++ frontend/src/app/flowchart/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FlowchartPage = () => {
+  return (
+    <div>
+      <h1>Flowchart</h1>
+    </div>
+  )
+}
+
+export default FlowchartPage
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <h1 className="text-4xl font-bold text-center">
+        Welcome to the Next.js + Tailwind CSS Starter!
+      </h1>
     </>
   );
 }
