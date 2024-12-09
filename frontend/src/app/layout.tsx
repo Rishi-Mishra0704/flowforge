171290bf66b5a757db316f803158d59a13ad9cf8
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-12-09 21:33:18 +0530

    nothing happened

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
-import Sidebar from "@/components/ui/Sidebar";
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <Sidebar>{children}</Sidebar>
       </body>
     </html>
   );
